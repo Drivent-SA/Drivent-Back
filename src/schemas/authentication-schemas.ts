d6b8fd8b7bb7ed5353775bb83e0740c95fa662a4
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-01-05 15:09:21 -0300

    feat: now the user can log in with github

--- src/schemas/authentication-schemas.ts
+++ src/schemas/authentication-schemas.ts
@@ -5,3 +5,11 @@ export const signInSchema = Joi.object<SignInParams>({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
+
+export const logInOauthSchema = Joi.object<logInOauthSchema>({
+  email: Joi.string().email().required()
+});
+
+type logInOauthSchema = {
+  email: string
+}
